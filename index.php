8577438f1bc9c5acda3b7dc953f74c29d2c04843
bahaztariq <bahaztariq@gmail.com>
2025-12-03 22:57:48 +0100

    Refactor header section to include logo and title for Budgy

--- index.php
+++ index.php
@@ -15,7 +15,10 @@
 <body class="bg-gray-100">
     <header class="fixed w-full">
         <div class="w-full p-4 flex justify-between items-center">
-        <img src="imgs/logo.png" alt="" class="w-42 h-16">
+        <div class="flex items-center">
+        <img src="imgs/icon.png" alt="" class="w-12 h-12">
+        <h2 class="text-black text-3xl font-bold">Budgy</h2>
+        </div>
         <a href="dashboard.php" class=" flex justify-center items-center bg-black text-white px-6 py-3 rounded-xl text-xl">Get Start</a>
        </div> 
     </header>
