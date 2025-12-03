3695a74d9932aed48fd619da52ef8edc1970c814
bahaztariq <bahaztariq@gmail.com>
2025-12-03 22:36:07 +0100

    Add tables for incomes and expenses in Smart Wallet database

--- Smart_Wallet.sql
+++ Smart_Wallet.sql
@@ -1,3 +1,16 @@
 CREATE DATABASE Smart_Wallet;
 
 USE Smart_Wallet;
+
+CREATE TABLE incomes(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    montant FLOAT NOT NULL,
+    description varchar(1000) NOT NULL,
+    date_ DATE DEFAULT (CURRENT_TIME)
+);
+CREATE TABLE expences(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    montant FLOAT NOT NULL,
+    description varchar(1000) NOT NULL,
+    date_ DATE DEFAULT (CURRENT_TIME)
+);
