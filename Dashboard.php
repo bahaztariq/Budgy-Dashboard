dcde6feab8e44337ad927343dc0b9a3f779605c6
bahaztariq <bahaztariq@gmail.com>
2025-12-04 09:45:18 +0100

    Add Dashboard page and database connection script

---
+++ Dashboard.php
@@ -0,0 +1,16 @@
+<?php
+require('db_connect.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard - budgy the best dashboard for Manage money</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"/>
+</head>
+<body>
+    
+</body>
+</html>
